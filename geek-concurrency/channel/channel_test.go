a9572500f2d5a1fd56a1f3b811f92ab5e6565431
Kallen <keleix@gmail.com>
2023-12-12 00:14:27 +0800

    feat: channel practise

---
+++ geek-concurrency/channel/channel_test.go
@@ -0,0 +1,63 @@
+package channel
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestChanelPrint(t *testing.T) {
+	chs := [4]chan struct{}{
+		make(chan struct{}),
+		make(chan struct{}),
+		make(chan struct{}),
+		make(chan struct{}),
+	}
+
+	for i := 0; i < 4; i++ {
+		go func(i int) {
+			for {
+				<-chs[i%4]
+				t.Logf("current groutine: %d", i)
+				time.Sleep(time.Second)
+				chs[(i+1)%4] <- struct{}{}
+			}
+		}(i)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	chs[0] <- struct{}{}
+	select {
+	case <-ctx.Done():
+		t.Logf("context done")
+	case <-time.After(time.Second * 30):
+		t.Logf("context timeout")
+	}
+}
+
+func TestChannelPlan(t *testing.T) {
+	num := 4
+	chs := make([]chan struct{}, num)
+	for i := 0; i < num; i++ {
+		chs[i] = make(chan struct{})
+	}
+
+	for i := 0; i < num; i++ {
+		go func(i int) {
+			for {
+				<-chs[i]
+				t.Logf("current groutine: %d", i)
+				time.Sleep(time.Second)
+				chs[(i+1)%num] <- struct{}{}
+			}
+		}(i)
+	}
+
+	chs[0] <- struct{}{}
+	select {
+	case <-time.After(time.Second * 20):
+		return
+	}
+}
