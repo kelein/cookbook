8f594c58d01a01b7eb4bf46dfb014894e7f400a4
kallen <keleix@gmail.com>
2023-12-26 19:12:04 +0800

    feat: grpc gateway implement

--- go.mod
+++ go.mod
@@ -9,27 +9,30 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/golang/protobuf v1.5.3
 	github.com/google/uuid v1.5.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.1
 	github.com/jedib0t/go-pretty/v6 v6.4.9
 	github.com/jinzhu/copier v0.4.0
 	github.com/olekukonko/tablewriter v0.0.5
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.8.4
 	golang.org/x/crypto v0.17.0
+	golang.org/x/net v0.17.0
 	golang.org/x/sync v0.5.0
-	google.golang.org/genproto/googleapis/api v0.0.0-20231002182017-d307bd883b97
+	google.golang.org/genproto/googleapis/api v0.0.0-20231106174013-bbf56f31fb17
 	google.golang.org/grpc v1.60.1
 	google.golang.org/protobuf v1.32.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	golang.org/x/net v0.16.0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto v0.0.0-20231002182017-d307bd883b97 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20231002182017-d307bd883b97 // indirect
+	google.golang.org/genproto v0.0.0-20231030173426-d783a09b4405 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231030173426-d783a09b4405 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- govoyage/cmd/server/server.go
+++ govoyage/cmd/server/server.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
 
 	"github.com/kelein/cookbook/govoyage/pbgen"
 	"github.com/kelein/cookbook/govoyage/service"
+)
+
+const (
+	contentHeader   = "Content-Type"
+	contentTypeGRPC = "application/grpc"
 )
 
 var port = flag.Int("port", 8080, "server listen port")
@@ -32,12 +44,13 @@ func init() {
 		os.Stdout, &slog.HandlerOptions{
 			AddSource:   true,
 			ReplaceAttr: replace,
+			// Level:       slog.LevelDebug,
 		},
 	))
 	slog.SetDefault(logger)
 }
 
-func main() {
+func mainV1() {
 	flag.Parse()
 	slog.Info("server start listen on", "port", *port)
 
@@ -55,4 +68,80 @@ func main() {
 		slog.Error("server listen failed", "error", err)
 	}
 	server.Serve(listener)
+}
+
+func main() {
+	flag.Parse()
+	slog.Info("server start listen on", "port", *port)
+
+	if err := RunMultiServer(*port); err != nil {
+		slog.Error("server start failed", "error", err)
+		os.Exit(1)
+	}
+}
+
+// RunMultiServer start both http and grpc server on the same port
+func RunMultiServer(port int) error {
+	addr := fmt.Sprintf(":%d", port)
+
+	httpMux := runHTTPServer()
+	grpcServer := runGRPCServer()
+	gatewayMux := runGRPCGateway(port)
+
+	httpMux.Handle("/", gatewayMux)
+	return http.ListenAndServe(addr, grpcHandlerFunc(grpcServer, httpMux))
+}
+
+// runHTTPServer starts a http server via grpc-gateway
+func runHTTPServer() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{"message": "HTTP server is running!"}`))
+	})
+	return mux
+}
+
+// runGRPCServer start a grpc server
+func runGRPCServer() *grpc.Server {
+	server := grpc.NewServer()
+	tager := service.NewTagService()
+	greeter := service.NewGreeterService()
+	pbgen.RegisterGreeterServer(server, greeter)
+	pbgen.RegisterTagServiceServer(server, tager)
+
+	// * Registe and grpcurl can test it
+	reflection.Register(server)
+	return server
+}
+
+// runGRPCGateway start a grpc gateway server
+func runGRPCGateway(port int) *runtime.ServeMux {
+	endpoint := fmt.Sprintf(":%d", port)
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(
+			insecure.NewCredentials(),
+		),
+	}
+	pbgen.RegisterGreeterHandlerFromEndpoint(context.Background(), mux, endpoint, opts)
+	pbgen.RegisterTagServiceHandlerFromEndpoint(context.Background(), mux, endpoint, opts)
+	return mux
+}
+
+func grpcHandlerFunc(grpcServer *grpc.Server, httpServer http.Handler) http.Handler {
+	grpcFn := func(w http.ResponseWriter, r *http.Request) {
+		ctype := r.Header.Get(contentHeader)
+		if r.ProtoMajor == 2 && strings.Contains(ctype, contentTypeGRPC) {
+			slog.Info("[GRPC]", "proto", r.Proto, "method", r.Method, "path", r.URL.String())
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			slog.Info("[HTTP]", "proto", r.Proto, "method", r.Method, "path", r.URL.String())
+			httpServer.ServeHTTP(w, r)
+		}
+	}
+
+	return h2c.NewHandler(
+		http.HandlerFunc(grpcFn),
+		&http2.Server{},
+	)
 }
