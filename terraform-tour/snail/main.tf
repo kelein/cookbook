0e675300897fa4bbe8dc706a714d5d2e83c5a7e7
kallen <keleix@gmail.com>
2024-08-12 17:10:30 +0800

    feat: add ASG defination

--- terraform-tour/snail/main.tf
+++ terraform-tour/snail/main.tf
@@ -42,3 +42,41 @@ output "public_ip" {
   value       = aws_instance.example.public_ip
   description = "The public IP of the instance"
 }
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+      #!/bin/bash
+      echo "Hello, World" > index.html
+      nohup busybox httpd -f -p 8080 &
+    EOF
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  min_size = 2
+  max_size = 5
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_ids = data.aws_vpc.default.id
+}
