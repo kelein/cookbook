afed15e6f0ca85d1239a615233ae2cb6ac525ed6
kallen <keleix@gmail.com>
2025-09-10 11:29:44 +0800

    feat: update makefile

--- gokite/main.go
+++ gokite/main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"log"
+	"net"
+
+	"github.com/cloudwego/kitex/server"
 
 	"github.com/kelein/cookbook/gokite/kitgen/service/echoservice"
 )
 
 func main() {
-	svr := echoservice.NewServer(new(EchoImpl))
+	serv := echoservice.NewServer(
+		new(EchoServiceFacade),
+		server.WithServiceAddr(&net.TCPAddr{
+			IP:   net.ParseIP("0.0.0.0"),
+			Port: 9000,
+		}),
+	)
 
-	err := svr.Run()
-
-	if err != nil {
+	if err := serv.Run(); err != nil {
 		log.Println(err.Error())
 	}
 }
